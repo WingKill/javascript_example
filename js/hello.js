8591f3d16471cdf0ace22162a67f3bc938d8d3bb
WingKill <zpswlsldi@gmail.com>
2024-03-19 17:48:20 +0900

    3번째 커밋 - 내장함수, 콜백함수, 객체, js파일에 수록해보기.

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,44 @@
-alert("안녕하세요");
+var calculator = (function(){
+    function add(x,y){
+        return x + y;
+    };
+
+    function sub(x,y){
+        return x - y;
+    };
+
+    function mul(x,y){
+        return x * y;
+    }
+
+    function div(x,y){
+        return x / y;
+    }
+    return {
+        add : add,
+        sub : sub,
+        mul : mul,
+        div : div,
+    };
+})();
+
+var area = (function(){
+    function getCircle(x){
+        return x * x * Math.PI;
+    }
+
+    function getRectangle(x,y){
+        return x * y;
+    }
+
+    function getTriangle(x,y){
+        return (x * y) / 2;
+    }
+
+    return {
+        getCircle : getCircle,
+        getRectangle : getRectangle,
+        getTriangle : getTriangle,
+    };
+})();
+//alert("안녕하세요");
